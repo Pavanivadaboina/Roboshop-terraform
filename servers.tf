8c5933621b9a5b02dd45f195266b01340a3ec3b6
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-01 16:36:39 +0530

    cart and common.sh code dry

--- servers.tf
+++ servers.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -8,7 +8,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_instance" "mongodb" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_instance" "mongodb" {
 
 
 resource "aws_instance" "catalogue" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -28,7 +28,7 @@ resource "aws_instance" "catalogue" {
 
 
 resource "aws_instance" "user" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -37,7 +37,7 @@ resource "aws_instance" "user" {
 }
 
 resource "aws_instance" "redis" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -46,7 +46,7 @@ resource "aws_instance" "redis" {
 }
 
 resource "aws_instance" "cart" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -55,7 +55,7 @@ resource "aws_instance" "cart" {
 }
 
 resource "aws_instance" "mysql" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -64,7 +64,7 @@ resource "aws_instance" "mysql" {
 }
 
 resource "aws_instance" "rabbitmq" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -73,7 +73,7 @@ resource "aws_instance" "rabbitmq" {
 }
 
 resource "aws_instance" "shipping" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -83,7 +83,7 @@ resource "aws_instance" "shipping" {
 
 
 resource "aws_instance" "payment" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
