846927ae3f5bd6695963912d2c80e26e1c348c02
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-01 17:23:26 +0530

    cart and common.sh code dry

--- servers.tf
+++ servers.tf
@@ -1,7 +1,12 @@
 data "aws_ami" "centos" {
-  owners           = ["973714476881"]
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
 
 
 resource "aws_instance" "frontend" {
