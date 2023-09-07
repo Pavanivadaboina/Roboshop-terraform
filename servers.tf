7744da0c3252ed96c883cb26500ed20857a99ed6
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-09-07 19:18:54 +0530

    terraform code

--- servers.tf
+++ servers.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "instance" {
-  for_each = var.components
+  for_each = "var.components"
   ami           = data.aws_ami.centos.image_id
 instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.default.id]
@@ -10,7 +10,7 @@ instance_type = each.value["instance_type"]
 }
 
 resource "aws_route53_record" "records" {
-  for_each = var.components
+  for_each = "var.components"
 
   zone_id = "Z01785992HNK9EWV1B0IH"
   name    = "${each.value["name"]}.devopspractice.tech"
